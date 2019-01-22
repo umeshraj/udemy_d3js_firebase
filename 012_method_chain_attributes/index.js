4d8ebf03e56c43a641b25fb5a9e23e4c669bf807
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2019-01-22 06:47:51 -0500

    Base code for lecture 12

---
+++ 012_method_chain_attributes/index.js
@@ -0,0 +1,9 @@
+const canvas = d3.select(".canvas");
+
+// appending to canvas
+const svg = canvas.append("svg");
+
+// append shapes to svg
+svg.append("rect");
+svg.append("circle");
+svg.append("line");
