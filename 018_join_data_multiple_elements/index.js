250d7d062661669632f780384cd142e5aa83e7cc
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2019-01-23 07:04:57 -0500

    Using selectAll to change multiple rects

--- 018_join_data_multiple_elements/index.js
+++ 018_join_data_multiple_elements/index.js
@@ -1,10 +1,16 @@
-const data = [{ width: 200, height: 100, fill: "purple" }];
+const data = [
+  { width: 200, height: 100, fill: "purple" },
+  { width: 100, height: 60, fill: "pink" },
+  { width: 50, height: 30, fill: "red" }
+];
 
 const svg = d3.select("svg");
 
 const rect = svg
-  .select("rect")
+  .selectAll("rect")
   .data(data)
   .attr("width", (d, i, n) => d.width)
   .attr("height", d => d.height)
   .attr("fill", d => d.fill);
+
+console.log(rect);
