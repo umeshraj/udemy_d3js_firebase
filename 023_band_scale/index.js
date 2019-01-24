d42469ac16f8915946662f9f19de09c0192bc197
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2019-01-24 07:47:18 -0500

    Using Bandscale for categorical axes

--- 023_band_scale/index.js
+++ 023_band_scale/index.js
@@ -9,22 +9,29 @@ d3.json("menu.json").then(data => {
     .domain([0, 1000])
     .range([0, 500]);
 
+  const x = d3
+    .scaleBand()
+    .domain(data.map(item => item.name))
+    .range([0, 500])
+    .paddingInner(0.2)
+    .paddingOuter(0.2);
+
   // join the data to a rect
   const rects = svg.selectAll("rect").data(data);
 
   // handle the existing rect
   rects
-    .attr("width", 50)
+    .attr("width", x.bandwidth)
     .attr("height", d => y(d.orders))
     .attr("fill", "orange")
-    .attr("x", (d, i) => i * 70);
+    .attr("x", d => x(d.name));
 
   // append the enter selection to dom
   rects
     .enter()
     .append("rect")
-    .attr("width", 50)
+    .attr("width", x.bandwidth)
     .attr("height", d => y(d.orders))
     .attr("fill", "orange")
-    .attr("x", (d, i) => i * 70);
+    .attr("x", d => x(d.name));
 });
