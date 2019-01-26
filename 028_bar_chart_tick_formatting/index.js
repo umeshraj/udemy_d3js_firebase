e2f6218e31751d95323912a048382c858d04cbea
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2019-01-26 08:08:56 -0500

    Change number of ticks, format and rotate text

--- 028_bar_chart_tick_formatting/index.js
+++ 028_bar_chart_tick_formatting/index.js
@@ -76,8 +76,18 @@ d3.json("menu.json").then(data => {
 
   // create and call the axes
   const xAxis = d3.axisBottom(x);
-  const yAxis = d3.axisLeft(y);
-  console.log(xAxis);
+  const yAxis = d3
+    .axisLeft(y)
+    .ticks(3)
+    .tickFormat(d => `${d} orders`);
+
   xAxisGroup.call(xAxis);
   yAxisGroup.call(yAxis);
+
+  // rotate x ticks
+  xAxisGroup
+    .selectAll("text")
+    .attr("transform", "rotate(-40)")
+    .attr("text-anchor", "end")
+    .attr("fill", "orange");
 });
