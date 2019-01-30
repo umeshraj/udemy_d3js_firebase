eb7912bcb7728ac6db0ae31b0eda6f2d927d6eae
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2019-01-30 07:47:56 -0500

    Adding item to firestore

--- 49_proj1_donut_add_data_firebase/index.js
+++ 49_proj1_donut_add_data_firebase/index.js
@@ -1,8 +1,39 @@
+let db;
 // Initialize Firebase
 fetch("apiKeyFirebase.json")
   .then(res => res.json())
   .then(config => {
     firebase.initializeApp(config);
-    const db = firebase.firestore();
+    db = firebase.firestore();
     console.log(db);
   });
+
+// various handles
+const form = document.querySelector("form");
+const name = document.querySelector("#name");
+const cost = document.querySelector("#cost");
+const error = document.querySelector("#error");
+
+// Add event listener for form submission
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  // validate inputs
+  if (name.value && cost.value) {
+    // create object to store in firestore
+    const item = {
+      name: name.value,
+      cost: parseInt(cost.value)
+    };
+
+    // add to firestore
+    db.collection("expenses")
+      .add(item)
+      .then(res => {
+        name.value = "";
+        cost.value = "";
+      });
+  } else {
+    // one of the fields is invalid
+    error.textContent = "Please enter values before submitting";
+  }
+});
