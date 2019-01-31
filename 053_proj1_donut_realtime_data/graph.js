d53efa083714f4363c4e6709177f5363765426c3
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2019-01-31 07:35:08 -0500

    Base code for 53

---
+++ 053_proj1_donut_realtime_data/graph.js
@@ -0,0 +1,34 @@
+// chart size
+const dims = { height: 300, width: 300, radius: 150 };
+const cent = { x: dims.width / 2 + 5, y: dims.height / 2 + 5 };
+
+// svg with 150px for legend
+const svg = d3
+  .select(".canvas")
+  .append("svg")
+  .attr("width", dims.width + 150)
+  .attr("height", dims.height + 150);
+
+// container for graph
+const graph = svg
+  .append("g")
+  .attr("transform", `translate(${cent.x}, ${cent.y})`);
+
+// pie generator
+const pie = d3
+  .pie()
+  .sort(null)
+  .value(d => d.cost);
+
+const angles = pie([
+  { name: "rent", cost: 500 },
+  { name: "bills", cost: 300 },
+  { name: "gaming", cost: 200 }
+]);
+
+// create arcs
+const arcPath = d3
+  .arc()
+  .outerRadius(dims.radius)
+  .innerRadius(dims.radius / 2);
+console.log(arcPath(angles[0]));
---
+++ 053_proj1_donut_realtime_data/index.js
@@ -0,0 +1,43 @@
+let db;
+// Initialize Firebase
+fetch("apiKeyFirebase.json")
+  .then(res => res.json())
+  .then(config => {
+    firebase.initializeApp(config);
+    db = firebase.firestore();
+    // console.log(db);
+  });
+
+// various handles
+const form = document.querySelector("form");
+const name = document.querySelector("#name");
+const cost = document.querySelector("#cost");
+const error = document.querySelector("#error");
+
+// Add event listener for form submission
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  // validate inputs
+  if (name.value && cost.value) {
+    // create object to store in firestore
+    const item = {
+      name: name.value,
+      cost: parseInt(cost.value)
+    };
+
+    // add to firestore
+    db.collection("expenses")
+      .add(item)
+      .then(res => {
+        // clear error message
+        error.textContent = "";
+
+        // clear input fields
+        name.value = "";
+        cost.value = "";
+      });
+  } else {
+    // one of the fields is invalid
+    error.textContent = "Please enter values before submitting";
+  }
+});
