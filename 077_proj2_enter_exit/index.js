39921ce0eb0b1078cc0719101d5aa8d279893571
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2019-02-06 08:01:30 -0500

    Base code for lecture 77

---
+++ 077_proj2_enter_exit/index.js
@@ -0,0 +1,52 @@
+// database initialize (config comes from import)
+firebase.initializeApp(config);
+const db = firebase.firestore();
+
+// DOM elements
+const btns = document.querySelectorAll("button");
+const form = document.querySelector("form");
+const formAct = document.querySelector("form span");
+const input = document.querySelector("input");
+const error = document.querySelector(".error");
+
+// event listeners for buttons
+let activity = "cycling";
+btns.forEach(btn => {
+  btn.addEventListener("click", e => {
+    // get activity
+    activity = e.target.dataset.activity;
+
+    // remove and add active class
+    btns.forEach(btn => btn.classList.remove("active"));
+    e.target.classList.add("active");
+
+    // set id of input field
+    input.setAttribute("id", activity);
+
+    // set text of form span
+    formAct.textContent = activity;
+  });
+});
+
+// form submit
+form.addEventListener("submit", e => {
+  e.preventDefault();
+
+  // get distance data
+  const distance = parseInt(input.value);
+  if (distance) {
+    db.collection("activities")
+      .add({
+        distance: distance,
+        activity: activity,
+        date: new Date().toString()
+      })
+      .then(() => {
+        error.textContent = "";
+        input.value = "";
+      });
+  } else {
+    // no distance
+    error.textContent = "Please enter a valid distance";
+  }
+});
