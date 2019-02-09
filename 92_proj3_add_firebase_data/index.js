074ba9e8c95d2d2c04be5a8fda697bea5bc60a8f
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2019-02-09 08:32:35 -0500

    Add database connection and add data

--- 92_proj3_add_firebase_data/index.js
+++ 92_proj3_add_firebase_data/index.js
@@ -1,2 +1,23 @@
 const modal = document.querySelector(".modal");
 M.Modal.init(modal);
+
+// database initialize (config comes from import)
+firebase.initializeApp(config);
+const db = firebase.firestore();
+
+// various elements
+const form = document.querySelector("form");
+const name = document.querySelector("#name");
+const parent = document.querySelector("#parent");
+const department = document.querySelector("#department");
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+
+  // add item to database
+  db.collection("employees").add({
+    name: name.value,
+    parent: parent.value,
+    department: department.value
+  });
+});
