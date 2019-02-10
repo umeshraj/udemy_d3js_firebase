c5bed802cb0b7e579094fa3c1fde2abe5a43a4d7
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2019-02-10 09:46:49 -0500

    Adding links

--- 97_proj3_links_enter_selection/graph.js
+++ 97_proj3_links_enter_selection/graph.js
@@ -30,6 +30,26 @@ const update = data => {
   // get node selection and join to elements/data
   const nodes = graph.selectAll(".node").data(treeData.descendants());
 
+  // get link selection and join data
+  const links = graph.selectAll(".link").data(treeData.links());
+  // console.log(treeData.links());
+
+  // enter new links
+  links
+    .enter()
+    .append("path")
+    .attr("class", "link")
+    .attr("fill", "none")
+    .attr("stroke", "#aaa")
+    .attr("stroke-width", 2)
+    .attr(
+      "d",
+      d3
+        .linkVertical()
+        .x(d => d.x)
+        .y(d => d.y)
+    );
+
   // create enter node groups
   const enterNodes = nodes
     .enter()
