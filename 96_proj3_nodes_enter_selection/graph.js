511cbc5334cd6d6cb18a1120d907c7c60070f05f
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2019-02-10 09:14:26 -0500

    Base code for lecture 96

---
+++ 96_proj3_nodes_enter_selection/graph.js
@@ -0,0 +1,63 @@
+// graph setup
+const dims = { height: 500, width: 1100 };
+
+const svg = d3
+  .select(".canvas")
+  .append("svg")
+  .attr("width", dims.width + 100)
+  .attr("height", dims.height + 100);
+
+const graph = svg.append("g").attr("transform", "translate(50, 50)");
+
+// data stratification
+const stratify = d3
+  .stratify()
+  .id(d => d.name)
+  .parentId(d => d.parent);
+
+const tree = d3.tree().size([dims.width, dims.height]);
+
+// update function
+const update = data => {
+  // get updated rootnode data
+  const rootNode = stratify(data);
+  // console.log(rootNode);
+
+  // creating tree
+  const treeData = tree(rootNode);
+  // console.log(treeData);
+
+  // get node selection and join to elements/data
+  const nodes = graph.selectAll(".node").data(treeData.descendants());
+};
+
+// data and firebase hook-up
+let data = [];
+
+// realtime listener
+db.collection("employees").onSnapshot(res => {
+  res.docChanges().forEach(change => {
+    const doc = { ...change.doc.data(), id: change.doc.id };
+
+    switch (change.type) {
+      case "added":
+        data.push(doc);
+        break;
+
+      case "modified":
+        const index = data.findIdex(item => item.id === doc.id);
+        data[index] = doc;
+        break;
+
+      case "removed":
+        data = data.filter(item => item.id !== doc.id);
+        break;
+
+      default:
+        break;
+    }
+  });
+
+  // console.log(data);
+  update(data);
+});
---
+++ 96_proj3_nodes_enter_selection/index.js
@@ -0,0 +1,30 @@
+const modal = document.querySelector(".modal");
+M.Modal.init(modal);
+
+// database initialize (config comes from import)
+firebase.initializeApp(config);
+const db = firebase.firestore();
+
+// various elements
+const form = document.querySelector("form");
+const name = document.querySelector("#name");
+const parent = document.querySelector("#parent");
+const department = document.querySelector("#department");
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+
+  // add item to database
+  db.collection("employees").add({
+    name: name.value,
+    parent: parent.value,
+    department: department.value
+  });
+
+  // close modal box
+  let instance = M.Modal.getInstance(modal);
+  instance.close();
+
+  // clear modal form inputs
+  form.reset();
+});
