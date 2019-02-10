59b29c81d63ed885f0ae6c1528d299e3975219c1
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2019-02-10 07:34:31 -0500

    Base code for lecture 95

---
+++ 95_proj3_tree_generator/graph.js
@@ -0,0 +1,40 @@
+// graph setup
+const dims = { height: 500, width: 1100 };
+
+const svg = d3
+  .select(".canvas")
+  .append("svg")
+  .attr("width", dims.width + 100)
+  .attr("height", dims.height + 100);
+
+const graph = svg.append("g").attr("transform", "translate(50, 50)");
+
+// data and firebase hook-up
+let data = [];
+
+// realtime listener
+db.collection("employees").onSnapshot(res => {
+  res.docChanges().forEach(change => {
+    const doc = { ...change.doc.data(), id: change.doc.id };
+
+    switch (change.type) {
+      case "added":
+        data.push(doc);
+        break;
+
+      case "modified":
+        const index = data.findIdex(item => item.id === doc.id);
+        data[index] = doc;
+        break;
+
+      case "removed":
+        data = data.filter(item => item.id !== doc.id);
+        break;
+
+      default:
+        break;
+    }
+  });
+
+  console.log(data);
+});
